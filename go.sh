9655decb2ee9f79c2334ba2a2a4f341033b1c20c
troldefar <r_emil@live.dk>
2024-08-11 12:43:42 +0200

    meh

--- go.sh
+++ go.sh
@@ -1,5 +1,7 @@
 sudo apt-get install ripgrep &&
+sudo apt-get install npm &&
 npm install -g typescript-language-server typescript && 
+sudo apt-get install composer &&
 composer global require felipegarcias/php-language-server &&
 sudo apt-get install ccls &&
 sudo apt-get install clangd
